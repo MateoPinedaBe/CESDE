8ffebc3c1e4e6b55f0ee24691d6c571d49aff4fe
Mateo Andres Pineda Beltran <mateo.pineda@globant.com>
2025-12-11 18:08:32 -0500

    Feat: Exercise Animals.

---
+++ tasks/animals/1- exercise.js
@@ -0,0 +1,35 @@
+import animals from './animals.js'
+
+// 1. create a function that returns the number of animals that have a specific color, the function should be named "getAnimalsByColor", 
+// the function should receive only one parameter (the color the animal should have)
+
+// if the animal's color is: "White, Brown" and the color that we are searching is "Brown" the animal meets the color requirement.
+
+// if the animal's color is "Red-brown" and the color that we are searching is "red" the animal meets the color requirement.
+
+// notes:
+
+// - there are 8 animals with the color "Brown"
+
+// - there is only 1 animal with the color "Blue"
+
+// - there are 2 animals with the color "Red"
+
+function getAnimalsByColor(color) {
+
+    color = String(color).toLowerCase();
+
+    let numAnimals = 0;
+
+    for (let i = 0; i < animals.length; i++) {
+
+        if (animals[i].color.toLowerCase().includes(color)) {
+            numAnimals++;
+        }
+
+    }
+
+    return numAnimals;
+}
+
+console.log(getAnimalsByColor("Brown"))
